ccb5f3a655a8c6791c1739cbab120ee3d9b49d90
Rob LaBonne <roblabonne@gmail.com>
2017-10-06 21:58:35 -0400

    checkpoint 6 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,34 +1,43 @@
 require_relative 'entry'
+require "csv"
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+   attr_reader :entries
 
-    def initialize
+   def initialize
       @entries = []
-    end
+   end
 
-  def add_entry(name, phone_number, email)
-    index = 0
-    entries.each do |entry|
-      if name < entry.name
-        break
+   def add_entry(name, phone_number, email)
+      index = 0
+      entries.each do |entry|
+         if name < entry.name
+            break
+         end
+         index += 1
       end
-      index += 1
-    end
 
-    entries.insert(index, Entry.new(name, phone_number, email))
-  end
+      entries.insert(index, Entry.new(name, phone_number, email))
+   end
 
-  def remove_entry(name, phone_number, email)
-    delete_entry = nil
+   def remove_entry(name, phone_number, email)
+      delete_entry = nil
 
-    entries.each do |entry|
-      if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        delete_entry = entry
+      entries.each do |entry|
+         if name == entry.name && phone_number == entry.phone_number && email == entry.email
+            delete_entry = entry
+         end
       end
-    end
 
-    entries.delete(delete_entry)
-  end
+      entries.delete(delete_entry)
+   end
 
+   def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+      csv.each do |row|
+         row_hash = row.to_hash
+         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+   end
 end
